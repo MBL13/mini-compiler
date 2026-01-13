1af9d3786a031ed30f9e71d05a9ee861af5454ca
Your Name <120112177+MundombeH@users.noreply.github.com>
2026-01-13 10:53:57 +0100

    revertidos antigos commits
    Revertido ate ao commit 50

--- src/semantic/Semantic.ts
+++ src/semantic/Semantic.ts
@@ -1,28 +1,8 @@
 import ASTNode from "../parser/IParser";
 
 interface Symbol {
-  name: string;
   value: number | string | boolean;
-  type: "INTEIRO" | "REAL" | "NATURAL" | "TEXTO" | "LOGICO" | "any";
-  // scope: "global" | "local";
-  
-}
-
-class Scope{
-    private symbols = new Map<string, Symbol> ();
-    add(symbol: Symbol): void{
-        if(this.symbols.has(symbol.name)){
-            throw new Error(`'${symbol.name}' a variável já foi declarada neste escopo`);
-        }
-        this.symbols.set(symbol.name, symbol);
-    }
-    lookup(name: string):Symbol | undefined{
-        return this.symbols.get(name);
-    }
-    //Remover escopo ao sair
-    remove(name: string): boolean{
-        return this.symbols.delete(name);
-    }
+  type: "INTEIRO" | "REAL" | "NATURAL" | "TEXTO" | "LOGICO";
 }
 
 /**
@@ -30,43 +10,7 @@ class Scope{
  * Ele mantém uma tabela de símbolos para armazenar valores e tipos das variáveis.
  */
 class SemanticAnalyzer {
-  // private simbols: Record<string, Symbol> = {};
-
-  private stackScopes: Scope[] = [];
-
-    //Inicio do escopo global
-    constructor(){
-        this.stackScopes.push(new Scope());
-    }
-
-    private currentEscope(): Scope{
-      if(this.stackScopes.length === 0){
-        throw new Error('Erro: pilha de escopos vazia.')
-      }
-      return this.stackScopes[this.stackScopes.length - 1]!;
-
-    }
-
-    //Entrar em um novo escopo
-    private enterScope(){
-        this.stackScopes.push(new Scope());
-    }
-    //Sair do escopo actual
-    private outScope(){
-        this.stackScopes.pop();
-    }
-
-    
-    private lookupSymbol(nome: string): Symbol | undefined{
-        for(let i = this.stackScopes.length - 1; i >= 0; i--){
-          const scope = this.stackScopes[i]!;
-            const symbol = scope.lookup(nome);
-            if(symbol){
-                return symbol;
-            }
-        }
-        throw new Error(`'${nome}' a variável não foi declarada.`);
-    }
+  private simbols: Record<string, Symbol> = {};
 
   /**
    * Executa a lista de comandos representada pela AST.
@@ -125,9 +69,7 @@ class SemanticAnalyzer {
             break;
         }
 
-         this.currentEscope().add({
-            name: node.id, value, type: node.varType
-        });
+        this.simbols[node.id] = { value, type: node.varType };
         break;
       // Comando print
       case "PrintStatement":
@@ -156,16 +98,7 @@ class SemanticAnalyzer {
       case "BooleanLiteral":
         return node.value;
 
-        //Escopo
-        case "Block":
-            this.enterScope();
-
-            for(const dec of node.declaration){
-                this.visit(dec);
-            }
-            this.outScope();
-            break;
-      //Booleano
+      //
 
       case "IfStatement": {
         const cond = this.visit(node.condition);
@@ -204,7 +137,7 @@ class SemanticAnalyzer {
 
       // Identificador
       case "IDENTIFICADOR":
-        const symbol = this.lookupSymbol(node.name);
+        const symbol = this.simbols[node.name];
         if (!symbol) {
           throw new Error(
             `Erro semântico: variavel ${node.name} não foi declarada`
